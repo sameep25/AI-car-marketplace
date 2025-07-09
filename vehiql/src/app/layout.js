87f8912c05caf6f93314d6b33438e9ca43a90f85
sameep25 <sameepvishwakarma@gmail.com>
2025-07-09 12:09:57 +0530

    adds clerk authentication and middleware

--- vehiql/src/app/layout.js
+++ vehiql/src/app/layout.js
@@ -1,6 +1,8 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
-import header from "@/components/header";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,18 +15,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className}`}>
-        <header />
+    // wrapping the app in clerk
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
 
-        <main className="min-h-screen">{children}</main>
+          <main className="min-h-screen">{children}</main>
 
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made by Sameep Vishwakarma</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made by Sameep Vishwakarma</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
