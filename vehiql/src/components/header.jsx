74821e7dcd0384fd78e1630edf97150fb2bae0e3
sameep25 <sameepvishwakarma@gmail.com>
2025-07-10 16:18:32 +0530

    created header component

--- vehiql/src/components/header.jsx
+++ vehiql/src/components/header.jsx
@@ -1,24 +1,83 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { ArrowLeft, CarFront, Heart, Layout } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-const Header = () => {
+const Header = async ({ isAdminPage = false }) => {
+  const isAdmin = false;
+
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
+    <header className="fixed top-0 w-full bg-white/80  backdrop-blur-md z-50 border-b">
+      <nav className="mx-auto px-4 py-4 flex items-center justify-between">
+        {/* CarLogo */}
+        <Link href={isAdminPage ? "/admin" : "/"} className="flex">
+          <Image
+            src={"/logo.png"}
+            alt="vehiql_logo"
+            width={100}
+            height={60}
+            className="object-contain h-12 w-auto"
+          />
+          {isAdminPage ? (
+            <span className="text-xs font-extralight">Admin</span>
+          ) : (
+            <></>
+          )}
+        </Link>
 
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+        <div className="flex items-center space-x-4">
+          {isAdminPage ? (
+            <Link href="/">
+              <Button variant="outline" className="flex items-center ">
+                <ArrowLeft size={18} />
+                <span className="hidden md:inline">Back to App</span>
+              </Button>
+            </Link>
+          ) : (
+            <SignedIn>
+              <Link href="/saved-cars">
+                <Button>
+                  <CarFront size={18} />
+                  <span className="hidden md:inline">Saved Cars</span>
+                </Button>
+              </Link>
+
+              {isAdmin ? (
+                <Link href="/admin">
+                  <Button variant="outline">
+                    <Layout size={18} />
+                    <span className="hidden md:inline">Admin Portal</span>
+                  </Button>
+                </Link>
+              ) : (
+                <Link href="/reservations">
+                  <Button variant="outline">
+                    <Heart size={18} />
+                    <span className="hidden md:inline">My Reservations</span>
+                  </Button>
+                </Link>
+              )}
+
+              <UserButton
+                appearance={{
+                  elements: {
+                    avatarBox: "w-15 h-15",
+                  },
+                }}
+              />
+            </SignedIn>
+          )}
+
+          <SignedOut>
+            <SignInButton>
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+        </div>
+      </nav>
+    </header>
   );
 };
 
