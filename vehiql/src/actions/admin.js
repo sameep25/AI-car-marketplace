19a9bd16634a633a0a8d5a22317e100432be338c
sameep25 <sameepvishwakarma@gmail.com>
2025-07-19 13:58:55 +0530

    Creates server-action for admin

---
+++ vehiql/src/actions/admin.js
@@ -0,0 +1,31 @@
+// server actions -> bascially API calls
+// All the admin related APIs
+
+"use server";
+
+import { db } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/dist/types/server";
+
+// fetch a admin user from a db through userId
+export async function getAdmin() {
+  // check is a user is logged in
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  // check is user exists in db
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  //   check if user does not exist in db or is not a admin
+  if (!user || user.role !== "ADMIN") {
+    return {
+      authorized: false,
+      reason: "not-admin",
+    };
+  }
+  return {
+    authorized: true,
+    user,
+  };
+}
