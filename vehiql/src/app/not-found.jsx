2592c79e528d7c8c2c9dc87d272e47aaf523615a
sameep25 <sameepvishwakarma@gmail.com>
2025-07-19 13:41:39 +0530

    UI changes in not-found page

--- vehiql/src/app/not-found.jsx
+++ vehiql/src/app/not-found.jsx
@@ -4,8 +4,7 @@ import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h[100vh] px-4 text-center">
-      not-found
+    <div className="flex flex-col items-center justify-center min-h[100vh] px-4 text-center my-32">
       <h1 className="text-6xl font-bold gradient-title mb-4">404</h1>
       <h2 className="text-2xl font-semibold mb-4">Page Not Found</h2>
       <p className="text-gray-600 mb-4">
