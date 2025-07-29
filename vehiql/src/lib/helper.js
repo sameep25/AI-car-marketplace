0f84e8a445b6d2d3964685da295eb887c00723d5
sameep25 <sameepvishwakarma@gmail.com>
2025-07-29 14:45:54 +0530

    Adds the format currency function

--- vehiql/src/lib/helper.js
+++ vehiql/src/lib/helper.js
@@ -1,9 +1,16 @@
 export const serializedCarsData = (car, wishliseted = "false") => {
   return {
     ...car,
-    price: car.price ? parseFloat(cars.price.toString()) : 0,
+    price: car.price ? parseFloat(car.price.toString()) : 0,
     createdAt: car.createdAt?.toISOString(),
     updatedAt: car.updatedAt?.toISOString(),
     wishliseted: wishliseted,
   };
+};
+
+export const formatCurrency = (amount) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(amount);
 };
