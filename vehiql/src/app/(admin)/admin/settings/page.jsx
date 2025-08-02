c5808e43f331100600f01e9857f3c940f058709c
sameep25 <sameepvishwakarma@gmail.com>
2025-08-02 15:13:12 +0530

    Adds the settings page

--- vehiql/src/app/(admin)/admin/settings/page.jsx
+++ vehiql/src/app/(admin)/admin/settings/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SettingsForm from "./_components/SettingsForm";
 
 export const metaData = {
   title: "Settings | Vehiql Admin",
@@ -9,6 +10,7 @@ const SettingsPage = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Settings</h1>
+      <SettingsForm />
     </div>
   );
 };
