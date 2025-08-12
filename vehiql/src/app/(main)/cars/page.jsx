a7a55c6bb51df5997d58dd1e65feaa64b61b7105
sameep25 <sameepvishwakarma@gmail.com>
2025-08-12 17:31:20 +0530

    Adds the cars page

---
+++ vehiql/src/app/(main)/cars/page.jsx
@@ -0,0 +1,33 @@
+import { getCarFilters } from "@/actions/car-listing";
+import React from "react";
+import CarFilters from "./_components/CarFilters";
+import CarListings from "./_components/CarListings";
+
+export const metaData = {
+  title: "Cars | Vehiql",
+  description: "Browse and search for your dream car",
+};
+
+const CarsPage = async () => {
+  const filtersData = await getCarFilters();
+  // console.log(filtersData);
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-6xl mb-4 gradient-title">Browse Cars</h1>
+
+      <div className="flex flex-col lg:flex-row gap-8">
+        <div className="w-full lg:w-80 flex-shrink-0">
+          {/* filters */}
+          <CarFilters filters={filtersData} />
+        </div>
+
+        <div className="flex-1">
+          {/* Listings */}
+          <CarListings />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CarsPage;
