a2de8d9a3a59f5c4c8cda4a5b31141b712549255
sameep25 <sameepvishwakarma@gmail.com>
2025-08-18 15:10:13 +0530

    Creates the testdrive page and test drive form

---
+++ vehiql/src/app/(main)/test-drive/[id]/_components/TestDriveForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestDriveForm = () => {
+  return <div>TestDriveForm</div>;
+};
+
+export default TestDriveForm;
---
+++ vehiql/src/app/(main)/test-drive/[id]/page.jsx
@@ -0,0 +1,32 @@
+import { getCarById } from "@/actions/cars";
+import { notFound } from "next/navigation";
+import React from "react";
+import TestDriveForm from "./_components/TestDriveForm";
+
+export async function generateMetaData() {
+  return {
+    title: "Book Test Drive | Vehiql",
+    description: "Schedule a test drive in few serconds",
+  };
+}
+
+const TestDrivePage = async ({ params }) => {
+  const { id } = params;
+  const result = await getCarById(id);
+
+  if (!result.success) {
+    notFound();
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-6xl mb-6 gradient-title">Book a Test-Drive</h1>
+      <TestDriveForm
+        car={result?.data}
+        testDriveInfo={result.data.testDriveInfo}
+      />
+    </div>
+  );
+};
+
+export default TestDrivePage;
